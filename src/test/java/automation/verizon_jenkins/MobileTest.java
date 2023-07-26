f3265390f64cee72d100dbac391e84222ed8b818
kastu <kasturajendran@gmail.com>
2023-07-26 14:10:32 +0530

    initial comment

---
+++ src/test/java/automation/verizon_jenkins/MobileTest.java
@@ -0,0 +1,20 @@
+package automation.verizon_jenkins;
+
+import org.testng.annotations.Test;
+
+
+public class MobileTest {
+	@Test
+	void login()
+	{
+		System.out.println("Login");
+	}
+	@Test
+	void logout()
+	{
+		System.out.println("logout");
+		//int num=10/0;
+	}
+
+}
+
